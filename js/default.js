d5f0399e277fd942549731a6b8bf8bb330e4f834
Dr.Jest <dc.jest@gmail.com>
2014-01-02 20:32:37 +0100

    Changed Online People Color

--- js/default.js
+++ js/default.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     if (mc!="default")
     {
       $color = $colors[mc];
-      var html = ('.%name% .news .nerdz_date, .%name% .news .post_icons, .%name% a, .%name% #profilePostArrow, .%name% #projectPostArrow, .%name% .spoiler span, .%name% .question {'+
+      var html = ('.%name% .news .nerdz_date, .%name% .news .post_icons, .%name% a, .%name% #profilePostArrow, .%name% #projectPostArrow, .%name% .spoiler span, .%name% .question, .%name% #followedlist .online {'+
                         '  color: %color%;'+
                         '}'+
                         '.%name% .news, .%name% .img_frame, .%name% .yt_frame, .%name% .window, .%name% .caption  {'+

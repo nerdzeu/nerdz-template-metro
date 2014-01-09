d66c8f17d4cdc9c54b069eb24177eab3969d1edb
Paolo Galeone <nessuno@nerdz.eu>
2014-01-09 11:03:20 +0100

    autolink fix

--- js/functions.js
+++ js/functions.js
@@ -99,7 +99,7 @@ if (!String.prototype.tag) {
 
 if(!String.prototype.autoLink) {
     String.prototype.autoLink = function() {
-    var pattern = /(?!\[(?:img|url|code|gist|yt|youtube|noparse)[^\]]*?\])(^|\s+)(((ht|f)tps?:\/\/)?([a-z\-]+\.)*[\-\w]+(\.[a-z]{2,4})+(\/[\w\_\-\?\=\#&\.]*)*(?![a-z]))(?![^\[]*?\[\/(img|url|code|gist|yt|youtube|noparse)\])/gi;
+    var pattern = /(?!\[(?:img|url|code|gist|yt|youtube|noparse)[^\]]*?\])(^|\s+)(((ht|f)tps?:\/\/)([a-z\-]+\.)*[\-\w]+(\.[a-z]{2,4})+(\/[\w\_\-\?\=\#&\.]*)*(?![a-z]))(?![^\[]*?\[\/(img|url|code|gist|yt|youtube|noparse)\])/gi;
     return this.replace(pattern, "$1[url]$2[/url]").replace(/\[(\/)?noparse\]/gi,"");
   };
 }
